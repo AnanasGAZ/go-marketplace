ace4345264c1a2dbd37db99465a094e006a716c6
Litavrina Anastasiya <a.a.litavrina@gmail.com>
2026-09-15 11:44:03 +0300

    add  order checkout

--- main.go
+++ main.go
@@ -326,3 +326,95 @@ func CalculateCartTotal(
 
 	return total, true
 }
+func Checkout(
+	users map[int]string,
+	balances map[int]int,
+	productPrices map[int]int,
+	productStocks map[int]int,
+	carts map[int]map[int]int,
+	orderOwners map[int]int,
+	orderItems map[int]map[int]int,
+	orderItemPrices map[int]map[int]int,
+	orderTotals map[int]int,
+	orderStatuses map[int]string,
+	operationHistory map[int][]string,
+	userID int,
+) (int, bool) {
+	// 1. Проверяем пользователя.
+	if _, userExists := users[userID]; !userExists {
+		return 0, false
+	}
+	// 2. Проверяем корзину: существует, не пустая
+	userCart, cartExists := carts[userID]
+	if !cartExists || len(userCart) == 0 {
+		return 0, false
+	}
+	// 3–5. Проверяем товары, остатки и  рассчитываем сумму.
+	total := 0
+	for productID, quantity := range userCart {
+		if quantity <= 0 {
+			return 0, false
+		}
+
+		price, productExists := productPrices[productID]
+		if !productExists {
+			return 0, false
+		}
+
+		stock, stockExists := productStocks[productID]
+		if !stockExists || quantity > stock {
+			return 0, false
+		}
+
+		total += price * quantity
+	}
+	// 6. Проверяем баланс.
+	balance, balanceExists := balances[userID]
+	if !balanceExists || balance < total {
+		return 0, false
+	}
+	//---проверки закончились, начинаем обрабатывать заказ---------
+
+	// 7. Создаём новый идентификатор заказа.
+	orderID := 1
+	for existingOrderID := range orderOwners {
+		if existingOrderID >= orderID {
+			orderID = existingOrderID + 1
+		}
+	}
+	// 8–9. Копируем товары и сохраняем цены.
+	items := map[int]int{}
+	itemPrices := map[int]int{}
+	//Заказ должен хранить отдельную копию из корзины ??? так?
+	for productID, quantity := range userCart {
+		items[productID] = quantity
+		itemPrices[productID] = productPrices[productID]
+	}
+
+	orderItems[orderID] = items
+	orderItemPrices[orderID] = itemPrices
+
+	// 10. Списываем деньги.
+	balances[userID] -= total
+
+	// 11. Уменьшаем остатки на складе.
+	for productID, quantity := range userCart {
+		productStocks[productID] -= quantity
+	}
+
+	// 12. Сохранить владельца, сумму и статус paid
+	orderOwners[orderID] = userID
+	orderTotals[orderID] = total
+	orderStatuses[orderID] = "paid"
+
+	// 13. Очищаем корзину.
+	carts[userID] = map[int]int{}
+
+	// 14. Добавляем запись в историю.
+	operationHistory[userID] = append(
+		operationHistory[userID],
+		fmt.Sprintf("order %d paid: %d", orderID, total),
+	)
+
+	return orderID, true
+}
